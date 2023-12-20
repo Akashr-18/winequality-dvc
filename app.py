f75b6c18ad3e1b759f6423e7d625628a507d6be9
Akashr-18 <akash.ravichandran18@gmail.com>
2023-12-20 17:57:02 +0530

    webapp updated

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+import os
+
+webapp_root = 'webapp'
+static_dir = os.path.join(webapp_root, 'static')
+template_dir = os.path.join(webapp_root, 'templates')
+
+app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
